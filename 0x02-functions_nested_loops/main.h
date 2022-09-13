06c4f9ff9cb5877ea5db4132b948bf4e31ea2b36
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-13 10:40:45 -0700

    low level c - program

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,7 +3,10 @@
 
 #include <stdio.h>
 
-int _putchar(char c);
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
