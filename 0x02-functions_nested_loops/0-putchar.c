1248e9fd1a87aa273c782b9cc33ee4f55d406e6c
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-13 09:48:46 -0700

    low level c program

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - entry point
+*
+* this code print to stdout _putchar
+* Return: Always 0 (success)
+*/
+int main(void)
+
+{
+	char str[] = "_putchar";
+	int ch;
+
+	for(ch = 0; ch < 8; ++ch);
+	_putchar(str[ch]);
+	_putchar('\n');
+return (0);
+}
