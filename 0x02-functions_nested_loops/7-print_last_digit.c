2d0d19c0c20f531010a2d822bd8850f4d38520de
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-14 06:13:49 -0700

    low level programming

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_last_digit - the function prints the last digit of s number
+* @x: input integer number
+* @i: last digit numbers
+* Return: the value of the last digit
+*/
+
+int print_last_digit(int i)
+{
+	int x;
+
+	x = (i % 10);
+	if (x < 0)
+	{
+		x = (-1 * x);
+	}
+	_putchar(x + '0')
+		return (x);
+
+}
