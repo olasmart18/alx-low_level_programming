3ffe6443c261500125fb3c503b7b617f593f0cd5
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-16 00:52:56 -0700

    low level programming

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,12 @@ void more_numbers(void)
 {
 	int i, j, c;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (c = 0; c <= 14; ++c)
 		{
 		j = c;
-			if (j > 14)
+			if (c > 9)
 		{
 			_putchar(1 + 48);
 			j = c % 10;
