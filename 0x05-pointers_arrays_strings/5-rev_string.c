d892b90c0bf818cf474a5888c8cc65ce16a263fe
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-21 00:42:04 -0700

    low level programming

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,5 +8,14 @@
 
 void rev_string(char *s)
 {
-	strrev(s);
+	int i, len;
+	char tmp;
+	len = strlen(s);
+
+	for (i = 0; i < len / 2; i++)
+	{
+		tmp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
+	}
 }
