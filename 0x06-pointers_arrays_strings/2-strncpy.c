945ff161f5cdcdc308aae8c011df4e0044caae71
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-22 16:56:15 -0700

    more of string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * strncpy - function copy strings
+ * @dest: destination string or first string
+ * @src: source string or second string
+ * @n: sring number to be copied
+ * 
+ * Return: 0
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+    int i;
+
+    for (i = 0; dest[i] != '\0'; i++)
+    {
+        if (i < n)
+        {
+             src[i] = dest[i];
+        }
+       
+    }
+    return (char*)src;
+}
