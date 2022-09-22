1277184698223ca988164c85ae7327a64d7e8986
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-22 10:06:45 -0700

    more of string

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,13 +4,12 @@
  * _strcat - function to concatenate 2 strings
  * @dest: destination string or first string
  * @src: source string or second string
- * Description: concatenate two string and return result on dest
+ *
  * Return: dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-    /*strcat(dest, src);*/
     int len1;
     int len2;
     int i;
