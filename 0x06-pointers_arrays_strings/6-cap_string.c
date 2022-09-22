b51c7453c261918ada348724b6100e8930b3d115
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-22 14:44:18 -0700

    more of function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -5,31 +5,31 @@
  * Separators of words: space, tabulation,
  * new line, ,, ;, ., !, ?, ", (, ), {, and }
  * @s: pointer to string
- * 
+ *
  * Return: pointer to s.
  */
 
 char *cap_string(char *s)
 {
-    int count;
+	int count;
 
-    while (s[count] != '\0')
-    {
-        if (s[0] >= 'a' && s[0] <= 'z')
-        {
-            s[0] = s[0] - 32;
-        }
-        if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
-        || s[count] == ',' || s[count] == ';' || s[count] == '.' 
-        || s[count] == '!' || s[count] == '"' || s[count] == '(' 
-        || s[count] == ')'|| s[count] == '{' || s[count] == '}')
-        {
-            if (s[count +1] >= 97; && s[count + 1] <= 122)
-            {
-                s[count + 1] = s[count + 1] - 32
-            }
-        }
-        count ++;
-    }
-    return(s);
+	while (s[count] != '\0')
+	{
+		if (s[0] >= 'a' && s[0] <= 'z')
+	{
+		s[0] = s[0] - 32;
+	}
+	if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+	|| s[count] == ',' || s[count] == ';' || s[count] == '.'
+	|| s[count] == '!' || s[count] == '"' || s[count] == '('
+	|| s[count] == ')' || s[count] == '{' || s[count] == '}')
+	{
+		if (s[count + 1] >= 97 && s[count + 1] <= 122)
+	{
+		s[count + 1] = s[count + 1] - 32
+	}
+	}
+	count++;
+	}
+	return (s);
 }
--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,30 +7,29 @@
  * Letters o and O should be replaced by 0
  * Letters t and T should be replaced by 7
  * Letters l and L should be replaced by 1
- * @s: pointer to string
- * 
+ * @str: pointer to string
+ *
  * Return: pointer to s.
  */
 
 char *leet(char *str)
 {
-    int stringcount = 0;
-    int leetcount = 0;
-    char leetAlpha[] = "aAeEoOtTlL";
-    char leetNum[] = "4433007711";
+	int stringcount = 0;
+	int leetcount = 0;
+	char leetAlpha[] = "aAeEoOtTlL";
+	char leetNum[] = "4433007711";
 
-    while (str[stringcount] != '\0')
-
-    {
-        while (leetcount < 10)
-        {
-            if (leetAlpha[leetcount] == str[stringcount)
-            {
-                str[stringcount] = leetNum[leetcount];
-            }
-            leetcount++;
-        }
-        stringcount++;
-    }
-    return(str);
+	while (str[stringcount] != '\0')
+	{
+		while (leetcount < 10)
+	{
+		if (leetAlpha[leetcount] == str[stringcount)
+	{
+		str[stringcount] = leetNum[leetcount];
+	}
+	leetcount++;
+	}
+	stringcount++;
+	}
+return (str);
 }
