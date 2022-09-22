bfdb32b05d81cdd71970b1274cc586fc9dfbc8cc
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-22 08:57:42 -0700

    more of string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - function to concatenate 2 strings
+ * @dest: destination string or first string
+ * @src: source string or second string
+ * Description: concatenate two string and return result on dest
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    strcat(dest, src);
+}
