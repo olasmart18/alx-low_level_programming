d61b0fa0ddb69db9e1e3e96559c9c2f421d0d842
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-22 19:01:42 -0700

    more of string

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalized every first letter in a string
+ * Separators of words: space, tabulation,
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }
+ * @s: pointer to string
+ * 
+ * Return: pointer to s.
+ */
+
+char *cap_string(char *s)
+{
+    int count;
+
+    while (s[count] != '\0')
+    {
+        if (s[0] >= 'a' && s[0] <= 'z')
+        {
+            s[0] = s[0] - 32;
+        }
+        if (s[count] == ' ' || s[count] == '\t' || s[count] == '\n'
+        || s[count] == ',' || s[count] == ';' || s[count] == '.' 
+        || s[count] == '!' || s[count] == '"' || s[count] == '(' 
+        || s[count] == ')'|| s[count] == '{' || s[count] == '}')
+        {
+            if (s[count +1] >= 97; && s[count + 1] <= 122)
+            {
+                s[count + 1] = s[count + 1] - 32
+            }
+        }
+        count ++;
+    }
+    return(s);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * leet - functuon encode into 1337
+ * Letters a and A should be replaced by 4
+ * Letters e and E should be replaced by 3
+ * Letters o and O should be replaced by 0
+ * Letters t and T should be replaced by 7
+ * Letters l and L should be replaced by 1
+ * @s: pointer to string
+ * 
+ * Return: pointer to s.
+ */
+
+char *leet(char *str)
+{
+    int stringcount = 0;
+    int leetcount = 0;
+    char leetAlpha[] = "aAeEoOtTlL";
+    char leetNum[] = "4433007711";
+
+    while (str[stringcount] != '\0')
+
+    {
+        while (leetcount < 10)
+        {
+            if (leetAlpha[leetcount] == str[stringcount)
+            {
+                str[stringcount] = leetNum[leetcount];
+            }
+            leetcount++;
+        }
+        stringcount++;
+    }
+    return(str);
+}
