a5938e60717266423d152ec8fe7b8c3370b35412
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-22 17:02:39 -0700

    more of string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,13 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
     int i;
 
-    for (i = 0; dest[i] != '\0'; i++)
+    for (i = 0; dest[i] == '\0' && src[i] == '\0'; i++)
     {
-        if (i < n)
+        if (i < n && src[i] != '\0')
         {
-             src[i] = dest[i];
+             dest[i] = src[i];
         }
        
     }
-    return (char*)src;
+    return (dest);
 }
