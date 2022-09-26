1a04c98220f9af5150752d5cc2bb060d91fe78c4
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-09-26 09:53:11 -0700

    2D arrays and more pointers

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strchr - the function locates a character in a string
+ * @s: string character
+ * @c: character to locate
+ * Return: a pointer to the first occurrence of the character c in the string s
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for(i = 0; i < s[i] && != '\0'; i++)
+	{
+		s[i] == c;
+		return (s);
+
+	} 
+	if c == '\0'
+	{
+		return (s);
+	}
+	return (NULL);
+}
