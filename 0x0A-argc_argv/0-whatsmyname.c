3d7fe78cdef4c8be4a55f73920313d9a1ac86970
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-01 01:02:46 -0700

    argc argv command line argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point for program to prints it name
+ * @argc: counter of argv passed in
+ * @argv: string passed in
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	/*for (i = 0; argc >= i; i++)*/
+		if (argc == 1)
+		{
+			printf("%s\n", argv[0]);
+		}
+	return (0);
+}
