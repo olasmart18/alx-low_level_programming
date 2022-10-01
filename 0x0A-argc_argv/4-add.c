98cf6c6b79d654654a638c714df069c735100d7e
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-01 11:07:35 -0700

    argc, argv

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,31 +1,44 @@
 #include "main.h"
 
 /**
+ * _isdigit - function for digit
+ * @s: string to check
+ * Return: 0 or 1
+ */
+
+int _isdigit(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+	}
+	return (0);
+}
+
+/**
  * main - entry point of program
  * @argc: counter of argv
  * @argv: argument
  * Return: 0 on successful
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char const *argv[])
 {
 	int sum = 0;
-	int i;
-	int j;
 
-	for (i = 0; i < argc; i++)
+	while (--argc)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
-		{
-		if (!isdigit(argv[i][j]))
+		if (_isdigit(argv[argc]))
 		{
 			printf("Error\n");
 			return (1);
 		}
-		}
 		/*atoi convert char passed in to int*/
-		sum = sum + atoi(argv[i]);
+		sum = sum + atoi(argv[argc]);
 	}
-	printf("%d\n", sum);
+	printf("%i\n", sum);
 	return (0);
 }
