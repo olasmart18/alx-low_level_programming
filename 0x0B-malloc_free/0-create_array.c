e7b0837297727f45494bcc64672ba927207e6246
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-03 04:33:14 -0700

    dynamic mem alloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * create_array - function that creates an array of chars
+ * @c: charactic to input
+ *
+ * Return: NULL if size = 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	int i;
+	char *ptr;
+	
+	ptr = (char*)malloc(i * sizeof(char));
+	if (ptr == NULL)
+	{
+		return (0);
+	}
+	return (0);
+}
