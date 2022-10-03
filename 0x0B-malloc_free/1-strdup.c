4b7fed9cf4b19f8090801c93173c9f6dccefd909
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-03 07:38:55 -0700

    malloc()

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strdup - function to copy and return pointer to new space
+ * @str: sring to copy
+ *
+ * Return:NULL if str = NULL
+ */
+
+
+char *_strdup(char *str)
+{
+	int i;
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ptr = (char *)malloc(i * sizeof(char));
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		str[i] = ptr;
+		if (str[i] == '\0')
+			return (ptr);
+		else
+			return (NULL);
+	}
+	return (0);
+}
