173264f9dff704b52c5462ddc0abb9e58f3f4106
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-04 02:46:10 -0700

    dynamic mem alloc

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * free_grid - function to free grid
+ * @grid: grid to free
+ * @height: height of grid
+ * Return: free grid
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i = 0;
+
+	while (i < height)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
