80bc6c61fe22ee242931f4e8314da08dedc4bb9d
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-06 11:39:39 -0700

    malloc()

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,7 +11,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int index, len1 = 0, len2 = 0;
+	unsigned int index = 0, len1 = 0, len2 = 0;
 	char *newMem;
 
 	if (s1 == NULL)
