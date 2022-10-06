52977d341693daf85b1679cb044646d80bac26ad
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-06 12:00:34 -0700

    malloc()

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -25,13 +25,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		len1++;
 	}
 
-	while (s2[len2] != '\0)
+	while (s2[len2] != '\0')
 	{
 		len2++;
 	}
 
 	if (n > len2)
-		n == len2;
+		n = len2;
 
 	newMem = malloc((len1 + n + 1) * sizeof(char));
 
