ac2277da1bda324f8a934cb6986cd7d3552dd15a
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-08 00:19:24 -0700

    malloc()

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+/**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+
+char *_memset(char *s, char b usigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
+}
+
+/**
+ * _calloc - function to allocate mem
+ * @nmemb: the number of mem
+ * @size: size of mem
+ * Return: pointer if size not 0
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *mem;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	mem = malloc(nmemb * size);
+
+	if (mem == NULL)
+	{
+		return (NULL);
+	}
+
+	_memset(mem, 0, (nmemb * size));
+
+	return (mem);
+}
