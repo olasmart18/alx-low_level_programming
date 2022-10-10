7788f002da9f7468c955f97bb5db29566df5bdd2
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-10 15:00:41 -0700

    structure in C

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include "main.h"
+
+/**
+ * new_dog - function that creates a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Return: pointer to new dog
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *dog;
+
+	dog = (malloc(sizeof(struct dog));
+
+			if (d == NULL)
+
+				return (NULL);
+			(*dog).name = name;
+			dog->owner = owner;
+		return (dog);
+}
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include "main.h"
+
+/**
+ * free_dog - function that frees dogs
+ * @d: struct dog
+ *
+ * Return: nothing
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
