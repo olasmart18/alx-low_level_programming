4a49e0e882d5ddfce1d5bd5e1ebe26b2e585b089
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-10 14:43:32 -0700

    preproccessor in C

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,11 @@
 #ifndef _PI_
 #define _PI_
 
-#define PI 3.14159265359
-
 /*
  * Create a header file that defines a macro named
  * PI as an abbreviation for the token 3.14159265359.
  */
+
+#define PI 3.14159265359
+
+#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,6 @@
-#define ABS(X) X
+#ifndef _FUNCTION_LIKE_MACRO_
+#define _FUNCTION_LIKE_MACRO_
+
+#define ABS(X) ((X) < (0) ? ((X) * (-1)) : (X))
+
+#endif
