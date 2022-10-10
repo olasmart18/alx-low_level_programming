3b81aff12836a79873594c9a885255c1f4241112
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-10 08:25:26 -0700

    structure in c

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,9 @@
+<! DOCTYPE html>
+<html>
+<head>
+<title>Readme</title>
+</head>
+<body>
+	<a href="https://en.wikipedia.org/wiki/Struct_(C_programming_language)">
+</body>
+</html>
---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int _putchar(char c);
+void free_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void print_dog(struct dog *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
