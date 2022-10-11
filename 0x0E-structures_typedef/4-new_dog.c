d1cb6d2fa87db3fd6c8cea44d4ce79e1ebf8e363
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-11 00:37:41 -0700

    structure in C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,14 +11,41 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *dog;
+	int iname, iowner, copy;
+	dog_t *n_dog;
 
-	dog = (malloc(sizeof(struct dog));
-
-			if (d == NULL)
-
+	n_dog = (malloc(sizeof(*n_dog));
+			if (n_dog == NULL || !(name) || !(owner))
+			{
+				free(n_dog);
 				return (NULL);
-			(*dog).name = name;
-			dog->owner = owner;
-		return (dog);
+			}
+			for (iname = 0; name[iname] != '\0'; iname++)
+			;
+
+			for (iowner = 0; owner[iowner] != '\0'; iowner++)
+			;
+
+			n_dog->nmae = malloc(iname * sizeof(n_dog->name));
+			if (n_dog->name == NULL)
+			{
+				free(n_dog->name);
+				free(n_dog);
+				return (NULL);
+			}
+			for (copy = 0; copy <= iname; copy++)
+				n_dog->name[copy] = name[copy];
+			n_dog->age = age;
+
+			n_dog->owner = malloc(iowner * sizeof(n_dog->owner));
+			if (n_dog->owner == NULL)
+			{
+				free(n_dog->owner);
+				free(n_dog->name);
+				free(n_dog);
+				return (NULL);
+			}
+			for (copy = 0; copy <= iowner; copy++)
+				n_copy->owner[copy] = owner[copy];
+		return (n_dog);
 }
