34e2243d455ad7a4eb9e5dc6c7c9ead2deae86c4
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-11 01:47:45 -0700

    structure in C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,46 +7,20 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
- * Return: pointer to new dog
+g* Return: pointer to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int iname, iowner, copy;
-	dog_t *n_dog;
+	dog_t n_dog;
+	dot_t *nw_dog = &n_dog;
 
-	n_dog = (malloc(sizeof(*n_dog));
-			if (n_dog == NULL || !(name) || !(owner))
-			{
-				free(n_dog);
-				return (NULL);
-			}
-			for (iname = 0; name[iname] != '\0'; iname++)
-			;
+	n_dog.name = name;
+	n_dog.age = age;
+	n_dog.owner = owner;
 
-			for (iowner = 0; owner[iowner] != '\0'; iowner++)
-			;
+	if (nw_dog == NULL)
+		return (NULL);
 
-			n_dog->nmae = malloc(iname * sizeof(n_dog->name));
-			if (n_dog->name == NULL)
-			{
-				free(n_dog->name);
-				free(n_dog);
-				return (NULL);
-			}
-			for (copy = 0; copy <= iname; copy++)
-				n_dog->name[copy] = name[copy];
-			n_dog->age = age;
-
-			n_dog->owner = malloc(iowner * sizeof(n_dog->owner));
-			if (n_dog->owner == NULL)
-			{
-				free(n_dog->owner);
-				free(n_dog->name);
-				free(n_dog);
-				return (NULL);
-			}
-			for (copy = 0; copy <= iowner; copy++)
-				n_copy->owner[copy] = owner[copy];
-		return (n_dog);
+	return (nw_dog);
 }
