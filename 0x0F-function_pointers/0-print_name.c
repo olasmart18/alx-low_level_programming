af4483c9d8fdd3003ee874c2c30b55e367af1800
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-12 10:46:19 -0700

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function to print a name
+ * @f: function pointer
+ * @name: name to print
+ *
+ * Return: point to function
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+
+	f(name);
+
+	name = malloc(sizeof(name));
+			if (!name || !f)
+			return;
+}
