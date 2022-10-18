93f0fbda11c97a0f0f0011497eb442540ab91a84
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-10-18 01:37:25 -0700

    variadic function

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - function that returns the sum of all its parameters
+ * @n: number of argument passed
+ *
+ * Return: sum of all digit
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int sum = 0;
+	int i;
+
+	va_list ptr; /* declaring point to the argument list*/
+	va_start(ptr, n);
+
+	if (n == 0)
+		return (0);
+
+	for (i = 0; i < n; i++)
+		sum += va_agr(ptr, int);
+	va_end(ptr);
+
+	return (sum);
+}
